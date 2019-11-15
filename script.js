883bb121014b737bb786df18960a52216ec2a237
michaelazee <54605024+michaelazee@users.noreply.github.com>
2019-11-15 16:39:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@
 
     if ( castRecording > 10 ) {
       $("#answer").text("Broadway, Here I Come!");
-      $("#broadway-img").attr("src", "https://media.giphy.com/media/QNUT5FSrWPtf7GqD5J/giphy.gif");
+      $("#img").attr("src", "https://media.giphy.com/media/QNUT5FSrWPtf7GqD5J/giphy.gif");
       
     } else {
       $("#answer").text("You can't sit with us :/");
-     var x = document.getElementById("myImg").attr("src", "https://cdn.vox-cdn.com/thumbor/MxOSpO68_0Rz9kqzmBVTOQA8ghc=/0x0:1600x1028/1200x0/filters:focal(0x0:1600x1028):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/10626485/MeanGirlsBway2539r_preview.jpeg");
+    $("#img").attr("src", "https://cdn.vox-cdn.com/thumbor/MxOSpO68_0Rz9kqzmBVTOQA8ghc=/0x0:1600x1028/1200x0/filters:focal(0x0:1600x1028):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/10626485/MeanGirlsBway2539r_preview.jpeg");
     }
   });
 
